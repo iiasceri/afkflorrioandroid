0721a676d29ce3b057530f1ac9c3eb288554d1d0
Nick Iascerinschi <nick.iasceri@gmail.com>
2023-06-14 16:18:32 +0300

    ...

--- androidApp/src/main/java/md/ciocana/iiasceri/afkflorrioandroid/android/MainActivity.kt
+++ androidApp/src/main/java/md/ciocana/iiasceri/afkflorrioandroid/android/MainActivity.kt
@@ -1,20 +1,12 @@
 package md.ciocana.iiasceri.afkflorrioandroid.android
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.webkit.WebViewAssetLoader
-import md.ciocana.iiasceri.afkflorrioandroid.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,23 +17,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val url = "https://www.google.com/"
-                    AndroidView(factory = {
-                        val wbAL = WebViewAssetLoader.Builder()
-                            .addPathHandler("/assets/", WebViewAssetLoader.AssetsPathHandler(this))
-                            .addPathHandler("/res/", WebViewAssetLoader.ResourcesPathHandler(this))
-                            .build()
-                        val webView = WebView(this)
-                        webView.apply {
-                            webViewClient = WebViewClient()
-
-                            settings.allowFileAccessFromFileURLs = true;
-                            settings.allowUniversalAccessFromFileURLs = true;
-
-                            loadUrl(url)
-                        }
-                        webView
-                    })
                 }
             }
         }
